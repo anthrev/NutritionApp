8ced4cc559c65946654f6601de81e94f139dc66d
Joshua <swansonj@mail.usf.edu>
2019-06-16 10:43:55 -0400

    Updated Food Diary Fragment

--- app/src/main/java/e/usf/nutritionapp/FoodDiaryFragment.java
+++ app/src/main/java/e/usf/nutritionapp/FoodDiaryFragment.java
@@ -7,12 +7,53 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FoodDiaryFragment extends Fragment {
+
+    TextView addBreakfastItem;
+    TextView addLunchItem;
+    TextView addDinnerItem;
+    TextView addSnackItem;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_food_diary,container,false);
+        View view = inflater.inflate(R.layout.fragment_food_diary,container,false);
+
+        addBreakfastItem = view.findViewById(R.id.add_food_breakfast);
+        addLunchItem = view.findViewById(R.id.add_food_lunch);
+        addDinnerItem = view.findViewById(R.id.add_food_dinner);
+        addSnackItem = view.findViewById(R.id.add_food_snack);
+
+        addBreakfastItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        addLunchItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        addDinnerItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        addSnackItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
 }
