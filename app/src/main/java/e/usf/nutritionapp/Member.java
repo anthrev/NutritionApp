203ffc433a94e35bce513032f09d9111a9c30a93
Joshua <swansonj@mail.usf.edu>
2019-06-16 09:42:25 -0400

    Member class created for later use with server

---
+++ app/src/main/java/e/usf/nutritionapp/Member.java
@@ -0,0 +1,12 @@
+package e.usf.nutritionapp;
+
+public class Member {
+    String goal;
+    String gender;
+    int height_feet;
+    int height_inches;
+    int weight;
+    int goal_weight;
+    int goal_weight_loss;
+
+}
