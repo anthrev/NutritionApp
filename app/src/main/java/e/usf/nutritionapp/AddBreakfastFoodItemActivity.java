ee7f49580d392139658bbbf2823d14d7ca6a856b
Joshua <swansonj@mail.usf.edu>
2019-06-16 10:44:48 -0400

    Added activity to add food items eaten for breakfast

---
+++ app/src/main/java/e/usf/nutritionapp/AddBreakfastFoodItemActivity.java
@@ -0,0 +1,15 @@
+package e.usf.nutritionapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AddBreakfastFoodItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_breakfast_food_item);
+
+    }
+
+}
