df7f0a1410db7ec5e80e6f6d7dbcd599a351c7f6
Joshua <swansonj@mail.usf.edu>
2019-06-16 09:46:52 -0400

    Home Fragment now contains path to Food Diary

--- app/src/main/java/e/usf/nutritionapp/HomeFragment.java
+++ app/src/main/java/e/usf/nutritionapp/HomeFragment.java
@@ -1,18 +1,38 @@
 package e.usf.nutritionapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class HomeFragment extends Fragment {
+
+    private static final String TAG = "HomeFragment";
+    private Button addMealButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        addMealButton = view.findViewById(R.id.add_meal_button);
+
+        Log.d(TAG,"onCreateView: started");
+
+        addMealButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment selectedFragment = new FoodDiaryFragment();
+                getFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+            }
+        });
+
+        return view;
     }
 }
