41cbba24525119b2fdb29cdbb50eca18292ba7f3
Joshua <swansonj@mail.usf.edu>
2019-06-16 09:45:14 -0400

    New Activity to question new users

---
+++ app/src/main/java/e/usf/nutritionapp/AfterRegistration.java
@@ -0,0 +1,13 @@
+package e.usf.nutritionapp;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class AfterRegistration extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
+
