e523b84a40d3bb062f7546f8e5311d142dc2a7a8
Joshua <swansonj@mail.usf.edu>
2019-06-16 03:25:11 -0400

    Implemented bottom nav bar

--- app/src/main/java/e/usf/nutritionapp/HomeActivity.java
+++ app/src/main/java/e/usf/nutritionapp/HomeActivity.java
@@ -1,7 +1,11 @@
 package e.usf.nutritionapp;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +13,38 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
     }
+
+    private  BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selectedFragment = null;
+
+                    switch(menuItem.getItemId()){
+                        case R.id.navigation_food_diary:
+                            selectedFragment = new FoodDiaryFragment();
+                            break;
+                        case R.id.navigation_goals:
+                            selectedFragment = new GoalsFragment();
+                            break;
+                        case R.id.navigation_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.navigation_more:
+                            selectedFragment = new MoreFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
