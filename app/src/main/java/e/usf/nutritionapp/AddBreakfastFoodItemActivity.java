9a899cc8f2ba4d14216339056a58c4ad89264c08
Joshua <swansonj@mail.usf.edu>
2019-06-16 17:50:58 -0400

    added search bar for food lookup

--- app/src/main/java/e/usf/nutritionapp/AddBreakfastFoodItemActivity.java
+++ app/src/main/java/e/usf/nutritionapp/AddBreakfastFoodItemActivity.java
@@ -1,15 +1,63 @@
 package e.usf.nutritionapp;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.mancj.materialsearchbar.MaterialSearchBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddBreakfastFoodItemActivity extends AppCompatActivity {
+
+    SearchView mySearchView;
+    String searchItem;
+
+    final String TAG = "AddBreakfastFoodItem";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_breakfast_food_item);
 
+        mySearchView = findViewById(R.id.searchView);
+
+
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                Log.d(TAG, ".onQueryTextSubmit");
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Log.d(TAG, ".onQueryTextChange");
+                return false;
+            }
+        });
     }
 
 }
